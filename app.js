75e7ddcaf6d77de1865a2c054eb56dadb045385e
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-07-15 17:09:18 +0530

    files added for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import blogRouter from './routes/blogRoutes.js';
 
 
 const app= express();
-const port=3000;
+const port= 3000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
